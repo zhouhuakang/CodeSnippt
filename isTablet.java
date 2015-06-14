6c0b7331f88007838b492bceeee50a0135a6a10c
周华康 <zhouhuakang459@gmail.com>
2015-06-14 16:43:47 +0800

    add the method  isTablet,which helps determine if the app is running in a Tablet context.

---
+++ isTablet.java
@@ -0,0 +1,11 @@
+/**
+     * Helps determine if the app is running in a Tablet context.
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isTablet(Context context) {
+        return (context.getResources().getConfiguration().screenLayout
+                & Configuration.SCREENLAYOUT_SIZE_MASK)
+                >= Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
